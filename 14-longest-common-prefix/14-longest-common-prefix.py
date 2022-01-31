41552d85d2a93f4882bc77ea2e10134ac0aa83f8
sankithack <sankitgamer@gmail.com>
2022-01-31 23:58:01 +0530

    Time: 43 ms (47.15%), Space: 14.1 MB (95.55%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,20 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        def check(str1,str2):
-            res=""
-            n1=len(str1)
-            n2=len(str2)
-            if n2==0 or n2==0:
-                return ""
-            for i in range(0,min(n1,n2)):
-                if(str1[i]==str2[i]):
-                    res+=str1[i]
-                else:
-                    return res
-            return res 
-        res=strs[0]
-        for i in strs:
-            res=check(res,i)
+        sorted_array=sorted(strs)
+        i=0
+        res=""
+        for i in range(0,len(sorted_array[0])):
+            if sorted_array[0][i]==sorted_array[-1][i]:
+                res+=sorted_array[0][i]
+            else:
+                return res
         return res
+            
+        
             
         
