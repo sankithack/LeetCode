fac042ee6b024c2a2de20b3a5734c0bcb7d50898
sankithack <sankitgamer@gmail.com>
2022-04-09 18:36:36 +0530

    Time: 10 ms (90.67%), Space: 44.6 MB (95.86%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        // O(1) time
+        if(k== nums.length)
+            return nums;
+        
+        int[] result=new int[k];
+        HashMap<Integer,Integer> map=new HashMap();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        PriorityQueue<Integer> minheap=new PriorityQueue<>(new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a,Integer b){
+                return map.get(a)-map.get(b);
+            }
+        });
+        for(int n:map.keySet()){
+            minheap.add(n);
+            if(minheap.size()>k)minheap.poll();
+        }
+        
+        for(int i=k-1;i>=0;i--){
+            result[i]=minheap.poll();
+        }
+        return result;
+    }
+}
